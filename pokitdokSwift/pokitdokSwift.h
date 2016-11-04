280c4361f7ac1de3695001db50f943fab62784fa
Charlie Thiry <charles.thiry@pokitdok.com>
2016-11-04 14:58:15 -0400

    refactor file structure

---
+++ pokitdokSwift/pokitdokSwift.h
@@ -0,0 +1,19 @@
+//
+//  pokitdokSwift.h
+//  pokitdokSwift
+//
+//  Created by Charlie Thiry on 11/4/16.
+//  Copyright © 2016 Charlie Thiry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for pokitdokSwift.
+FOUNDATION_EXPORT double pokitdokSwiftVersionNumber;
+
+//! Project version string for pokitdokSwift.
+FOUNDATION_EXPORT const unsigned char pokitdokSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pokitdokSwift/PublicHeader.h>
+
+
